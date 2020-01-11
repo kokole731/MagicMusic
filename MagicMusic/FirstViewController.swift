29f5be2d6cafb709e55da3f9c8ca0ab9c2bbdaaf
Chandller <807479144@qq.com>
2020-01-11 18:36:09 +0800

    Initial Commit

---
+++ MagicMusic/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MagicMusic
+//
+//  Created by Chandler on 2020/1/11.
+//  Copyright © 2020 Chandler. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
